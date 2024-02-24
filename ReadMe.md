65afa6ac8f874c2271a9db347b75485af3633c8b
Francisca Masombo <fran@Frans-MacBook-Pro-2.local>
2024-02-24 22:12:07 +0000

    Shop-Filter By Price Functionality

--- ReadMe.md
+++ ReadMe.md
@@ -1,9 +1,23 @@
-Project Setup:
+## Project Setup:
+``` npm
 npm install
 npm -i init
 npm install --save-dev cypress
-run npx cypress open 
-Then select E2E testing 
-Continue 
-Then select browser
-then Create new spec 
+
+# Then run npx cypress open 
+Then select E2E testing after that select the Continue button make sure you select a browser to run the test in. Create new spec and start coding.
+
+# Add /// <reference types="Cypress" /> to support/commands.js in order to use Cypress libriary 
+
+# In order to use xpath install cypress xpath (element selecter)
+npm install -D cypress-xpath
+Add require('cypress-xpath') to support/e2e.js 
+
+# To run tests for all specs or from a single spec file add the following to package.json
+ "scripts": {
+    "cy:run": "cypress run"
+  }
+Use npx cypress run to run tests 
+
+
+```
--- cypress/e2e/shop_spec.cy.js
+++ cypress/e2e/shop_spec.cy.js
@@ -1,5 +1,36 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('The Shop tests spec', () => {
+  beforeEach(() => {
+    // Visit the URL before each test
+    cy.visit('https://practice.automationtesting.in/shop/')
+    cy.wait(1000) //wait for the URl to load
+
+  });
+
+  // test case one
+  // Error with filter as the filter is not in order 
+  it('1.Shop-Filter By Price Functionality	', () => {
+    cy.xpath('//*[@id="woocommerce_price_filter-2"]/form/div/div[2]/div[1]/span[2]').invoke('text', '₹450'); // set price to 150 to450
+    cy.wait(2000) // wait for the new price to load 
+    cy.xpath('//*[@id="woocommerce_price_filter-2"]/form/div/div[2]/button').click(); // click filter to load results 
+    
+    // filter from high to low
+    cy.xpath('//*[@id="content"]/form/select').select('price-desc');
+    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span[2]/ins/span').invoke('text') 
+      .then((value) => {
+        cy.log(`The Highest is: ${value}`); // Log the value 
+      });
+    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span[2]/ins/span').should('include.text', '450'); //the highest cost should be 450
+    
+    // filter from low to high
+    cy.wait(1000)
+    cy.xpath('//*[@id="content"]/form/select').select('price'); 
+    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span/span').invoke('text') 
+      .then((value) => {
+        cy.log(`The lowest is: ${value}`); // Log the value 
+      });
+    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span/span').should('include.text', '150'); //the highest cost should be 450
   })
+
+  
 })
