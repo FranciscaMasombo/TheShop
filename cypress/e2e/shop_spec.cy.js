2dc344a3af3a9b51d91828a84b1ba7645c2c1ae1
FranciscaMasombo <frmm97@gmail.com>
2024-02-25 03:41:11 +0000

    10.Shop-Add to Basket-View Basket Functionality

--- cypress/e2e/shop_spec.cy.js
+++ cypress/e2e/shop_spec.cy.js
@@ -19,10 +19,9 @@ describe('The Shop tests spec', () => {
 
     // filter from high to low
     cy.xpath('//*[@id="content"]/form/select').select('price-desc');
-    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span[2]/ins/span').invoke('text')
-      .then((value) => {
-        cy.log(`The Highest is: ${value}`); // Log the value 
-      });
+    cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span[2]/ins/span').invoke('text').then((value) => {
+      cy.log(`The Highest is: ${value}`); // Log the value 
+    });
     cy.xpath('//*[@id="content"]/ul/li[1]/a[1]/span[2]/ins/span').should('include.text', '450'); //the highest cost should be 450
 
     // filter from low to high
@@ -108,7 +107,44 @@ describe('The Shop tests spec', () => {
   })
 
   it('10.Shop-Add to Basket-View Basket Functionality', () => {
-    // there is no Read more button on the Shop page 
+
+    cy.get('.products').should('be.visible');
+    cy.xpath('/html/body/div[1]/div[2]/div/div/ul/li[1]/a[2]').click(); //add the first book to basket 
+    cy.wait(1500);
+    cy.xpath('//*[@id="wpmenucartli"]/a').click(); // view basket
+    // Verify total and subtotal values
+    cy.xpath('//*[@id="page-34"]/div/div[1]/div/div/table/tbody/tr[1]/td/span').invoke('text').then((subtotal) => {
+      cy.xpath('//*[@id="page-34"]/div/div[1]/div/div/table/tbody/tr[3]/td/strong/span').invoke('text').then((total) => {
+        cy.log(`subtotal: ${subtotal}, total: ${total}`); // log the prices    
+        expect(parseFloat(subtotal.replace('₹', ''))).to.be.lessThan(parseFloat(total.replace('₹', '')));      //total is less than subtotal
+      });
+      cy.wait(1500);
+      cy.contains('Proceed to Checkout').click();
+      // fill in billing details form 
+      cy.get('#billing_first_name').type('Sarah');
+      cy.get('#billing_last_name').type('Smith');
+      cy.get('#billing_email').type('sarah.smith@doctor.com');
+      cy.get('#billing_phone').type('0892348797');
+      cy.get('#select2-chosen-1').type('Republic of Ireland');
+      cy.xpath('//*[@id="select2-result-label-433"]').click();
+      cy.get('#billing_address_1').type('Waterford');
+      cy.get('#billing_city').type('Waterford');
+      cy.get('#billing_state').type('Waterford');
+
+      cy.get('#payment_method_bacs').check(); //select the payment method Direct Bank Transfer
+      cy.wait(1500);
+      cy.contains('Place order').click();
+
+      // confirmed the order was made   
+      cy.contains('Thank you. Your order has been received.').should('be.visible');
+      cy.wait(1500);
+      cy.xpath('//*[@id="page-35"]/div/div[1]/h2[1]').should('be.visible'); //Bank details should be visable 
+      cy.xpath('//*[@id="page-35"]/div/div[1]/h2[2]').should('be.visible'); //Order details should be visable 
+      cy.xpath('//*[@id="page-35"]/div/div[1]/table/tfoot/tr[3]/td').should('be.visible');
+      cy.xpath('//*[@id="page-35"]/div/div[1]/table/tfoot/tr[3]/td').should('include.text', 'Direct Bank Transfer');
+
+    });
+
   })
 
 })
